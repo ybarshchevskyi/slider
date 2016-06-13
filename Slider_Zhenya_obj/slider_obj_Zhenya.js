9a4c6417a8a3676d49571d21bc223d7d0d424707
ybarshchevskyi <ybarshchevskyi@ndi.org>
2016-06-13 14:36:31 +0300

    Some changes

--- Slider_Zhenya_obj/slider_obj_Zhenya.js
+++ Slider_Zhenya_obj/slider_obj_Zhenya.js
@@ -2,9 +2,7 @@ function Slider (obj){
     var slides = $('#'+obj.id).find('.slider-item');
     var self = this;
     var ElemPosition = obj.move;
-//Пока что не понятно что делать с этим свойством    
-    var ElemProperty = obj.property;
-//.......
+    var action;
     this.init = function(){
 //Выстраивание картинок в линию
         for(var i = 0; i<slides.length; i++){
@@ -17,38 +15,28 @@ function Slider (obj){
             self[action]();
         });
     }
-//Метод движения слайдера влево
-    this.moveLeft = function(){
-        for(var i = 0; i<slides.length; i++){
-            var temporary = parseInt($(slides[i]).width());
-            var x = parseInt($(slides[i]).css(ElemPosition));
-            $(slides[i]).css(ElemPosition, (x+temporary)+'px');
-        }
-    }
-//Метод движения слайдера вправо
-    this.moveRight = function(){
-        for(var i = 0; i<slides.length; i++){
-            var temporary = parseInt($(slides[i]).width());
-            var x = parseInt($(slides[i]).css(ElemPosition));
-            $(slides[i]).css(ElemPosition, (x-temporary)+'px');
-        }
-    }
-    function getMove (array,button){
-        if(button === undefined){
-            
-        }
+this.moveLeft = function(){
+    getPosition(slides, '1');
+}
+
+this.moveRight = function(){
+    getPosition(slides, '2');
+}
+    function getPosition (array, trigger){
+        for(var i = 0; i<array.length; i++){
+            var temporary = parseInt($(array[i]).width());
+            var x = parseInt($(array[i]).css(ElemPosition));
+            var coord;
+        switch(trigger){
+            case '1':   
+                var coord = (x+temporary)+'px';
+                break;
         
-        switch(direction){
-            case : 'moveLeft'
-        {
-                
-                break
-        }
-            case : 'moveRight'
-            {
-                
-                break
+            case '2':
+                var coord = (x-temporary)+'px';
+                break;
+                }
+             $(array[i]).css(ElemPosition, coord);
             }
         }
     }
-}
